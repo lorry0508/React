ff5cb45c18d4ff871a3dca2935120cc712360b05
lorry0508 <1165354824@qq.com>
2020-09-22 11:26:56 +0800

    react中的组件

--- PC/todolist/src/App.js
+++ PC/todolist/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function App() {
-	return (
-		<div>
-			Hello, React!
-		</div>
-	);
+// JSX 语法
+class App extends React.Component {
+	render() {
+		return (
+			<div>Hello React!</div>
+		);
+	}
 }
 
 export default App;
