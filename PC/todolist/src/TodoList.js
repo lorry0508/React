e304de7a06b000e547538c5149923923678a16c8
lorry0508 <1165354824@qq.com>
2020-09-25 15:20:55 +0800

    创建redux中的store

--- PC/todolist/src/TodoList.js
+++ PC/todolist/src/TodoList.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import store from './store/index';
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+    }
     render() {
         return (
             <div style={{ marginTop: '10px', marginLeft: '10px' }}>
                 <div>
-                    <Input placeholder="todo info" style={{ width: '300px', marginRight: '10px' }} />
+                    <Input placeholder="todo info" style={{ width: '300px', marginRight: '10px' }} value={this.state.inputValue} />
                     <Button type="primary">提交</Button>
                 </div>
                 <List
                     style={{marginTop: '10px', width: '300px'}}
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (
                         <List.Item>
                             {item}
