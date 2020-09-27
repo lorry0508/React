47d8b213de1d0a4738fc8c9fc2753371a22b168c
lorry0508 <1165354824@qq.com>
2020-09-27 08:00:47 +0800

    actionCreator统一创建action

--- PC/todolist/src/TodoList.js
+++ PC/todolist/src/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store';
-import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreator';
 
 class TodoList extends React.Component {
     constructor(props) {
@@ -14,26 +14,18 @@ class TodoList extends React.Component {
         store.subscribe(this.handleStoreChange)
     }
     handleInputChange(e) {
-        const action = {
-            type: CHANGE_INPUT_VALUE,
-            value: e.target.value
-        };
+        const action = getInputChangeAction(e.target.value);
         store.dispatch(action);
     }
     handleStoreChange() {
         this.setState(store.getState());
     }
     handleBtnClick() {
-        const action = {
-            type: ADD_TODO_ITEM
-        };
+        const action = getAddItemAction();
         store.dispatch(action);
     }
     handleItemDelete(index) {
-        const action = {
-            type: DELETE_TODO_ITEM,
-            index
-        };
+        const action = getDeleteItemAction(index)
         store.dispatch(action);
     }
     render() {
