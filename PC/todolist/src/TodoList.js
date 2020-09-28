9f0d5a502a1121aece4fc3688932a098c2033595
lorry0508 <1165354824@qq.com>
2020-09-28 09:00:50 +0800

    使用react-redux实现TodoList功能

--- PC/todolist/src/TodoList.js
+++ PC/todolist/src/TodoList.js
@@ -1,33 +1,29 @@
 import React, { Fragment } from 'react';
-import store from './store';
 import { connect } from 'react-redux';
 
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-    }
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <input value={this.props.inputValue} onChange={this.props.handleInputChange} />
-                    <button>提交</button>
-                </div>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                </ul>
-            </Fragment>
-        );
-    }
+const TodoList = (props) => {
+    const { inputValue, handleInputChange, handleClick, list} = props;
+    return (
+        <Fragment>
+            <div>
+                <input value={inputValue} onChange={handleInputChange} />
+                <button onClick={handleClick}>提交</button>
+            </div>
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </Fragment>
+    );
 }
 
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     };
 };
 
@@ -38,6 +34,12 @@ const mapDispatchToProps = (dispatch) => {
             const action = {
                 type: 'change_input_value',
                 value: e.target.value
+            };
+            dispatch(action);
+        },
+        handleClick() {
+            const action = {
+                type: 'add_item'
             };
             dispatch(action);
         }
--- PC/todolist/src/store/reducer.js
+++ PC/todolist/src/store/reducer.js
@@ -1,12 +1,18 @@
 const defaultState = {
-    inputValue: '2313',
+    inputValue: '',
     list: []
 };
 
 export default (state = defaultState, action) => {
-    if(action.type === 'change_input_value') {
+    if (action.type === 'change_input_value') {
         const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'add_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
         return newState;
     }
     return state;
