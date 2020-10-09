e06b0e4e51638909623d1174a786f88152f5015f
lorry0508 <1165354824@qq.com>
2020-10-09 11:46:58 +0800

    styled-components完成header组件布局

--- PC/jianshu/src/common/header/index.js
+++ PC/jianshu/src/common/header/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { HeaderWrappper, Logo, Nav } from './style';
+import { 
+    HeaderWrappper, 
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
+} from './style';
 
 class Header extends React.Component {
     render() {
         return (
             <HeaderWrappper>
                 <Logo />
-                <Nav></Nav>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载APP</NavItem>
+                    <NavItem className='right'>登陆</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>写文章</Button>
+					<Button className='reg'>注册</Button>
+                </Addition>
             </HeaderWrappper>
         )
     }
