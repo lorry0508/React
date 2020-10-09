15cd35a9f79c7042b9be0c2b76d8e3f0aa3c6719
lorry0508 <1165354824@qq.com>
2020-10-09 09:19:37 +0800

    jianshu项目初始化，style-components和reset,css的使用

--- PC/coding-229/06-01/src/App.js
+++ PC/coding-229/06-01/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='dell'>
-        hello world
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='dell'>
+				hello world
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ PC/jianshu/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			learn react
+		</div>
+	);
+}
+
+export default App;
