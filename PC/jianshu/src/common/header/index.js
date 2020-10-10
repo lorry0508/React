701c4623f88c7a19ba2398b0ceb13b2b2955c644
lorry0508 <1165354824@qq.com>
2020-10-10 08:56:27 +0800

    actionCreators与constants的拆分

--- PC/jianshu/src/common/header/index.js
+++ PC/jianshu/src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store'
 import {
     HeaderWrappper,
     Logo,
@@ -21,16 +22,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus() {
-            const action = {
-                type: 'search_focus'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchFocus());
         },
         handleInputBlur() {
-            const action = {
-                type: 'search_blur'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
         }
     }
 };
--- PC/jianshu/src/common/header/store/index.js
+++ PC/jianshu/src/common/header/store/index.js
@@ -1,3 +1,5 @@
 import reducer from './reducer';
+import * as actionCreators from './actionCreators';
+import * as constants from './constants';
 
-export { reducer };
+export { reducer, actionCreators, constants }; // 尽量只使用一个出口文件
