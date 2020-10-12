87644868edf61adfa517eccaddb92079b084cac8
lorry0508 <1165354824@qq.com>
2020-10-12 14:12:23 +0800

    在React中使用路由功能

--- PC/jianshu/README.md
+++ PC/jianshu/README.md
@@ -4,3 +4,6 @@
     2.使用reset.css做到全局样式在各浏览器的统一
     3.immutable.js库，用于解决state是原始js对象，一旦被修改，对应原始对象也会被修改，使用深拷贝也会带来性能的损耗；所以使用immutable将js对象转换成immutable对象，能够很好的解决这些问题；immutable对象的get()和set()方法，immutable对象的set方法，会结合之前的immutable对象的值和设置的值，返回一个新的对象
     4.redux-thunk是redux的中间间，使得能够在action中去写函数
+    5.使用react-router-dom中的BrowserRouter和Route进行路由实现，BrowserRouter中只能有一个元素
+    6.Provider中尽量只包裹一个元素
+    7.Router路由标签中，使用exact表示路由的唯一性
--- PC/jianshu/src/App.js
+++ PC/jianshu/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header/index';
 import store from './store';
 
 function App() {
 	return (
 		<Provider store={store}>
-			<Header />
+			<div>
+				<Header />
+				<BrowserRouter>
+					<div>
+						<Route path='/' exact render={() => <div>home</div>}></Route>
+						<Route path='/detail' exact render={() => <div>detail</div>}></Route>
+					</div>
+				</BrowserRouter>
+			</div>
 		</Provider>
 	);
 }
